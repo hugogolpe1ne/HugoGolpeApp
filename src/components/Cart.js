afaf4597c242fca744d2e57dbe2996a9b17187d1
hugo <hugo.golpe.1@gmail.com>
2022-07-24 20:59:01 -0300

    trabajo final

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,75 @@
-import { serverTimestamp } from "firebase/firestore"
+import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { cartContext } from "./CartContext";
+
+const Cart = () => {
+  const resultContext = useContext(cartContext);
+
+  return (
+    <>
+      {resultContext.carrito.length < 1 ? (
+        <div>
+          <p>Su carrito se encuentra vacio</p>
+          <Link to="/">
+            Ver Productos
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <div>
+              <div></div>
+              <h5>Producto</h5>
+              <h5>Cantidad</h5>
+              <h5>Precio</h5>
+              <h5>Subtotal</h5>
+            </div>
+
+            <div>
+              {resultContext.carrito.map((producto) => (
+                <div key={producto.id}>
+                  <div>
+                    <img
+                      src={producto.pictureUrl}
+                      alt={`${producto.id} - ${producto.title}`}
+                    />
+                  </div>
+                  <div>{producto.name}</div>
+                  <div>{producto.cantidad_total}</div>
+                  <div>${producto.price}</div>
+                  <div>
+                    ${parseFloat((producto.price * producto.cantidad_total).toFixed(2))}
+                  </div>
+                  <button
+                    onClick={() => resultContext.eliminarProducto(producto.id)}
+                  >
+                    <span>
+                      delete
+                    </span>
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div >
+            <h2>Resumen de compra</h2>
+            <p>Subtotal: ${resultContext.precio_total}</p>
+            <p>Total: ${resultContext.precio_total}</p>
+            <hr></hr>
+            <Link to="/checkout">
+              <button >Iniciar compra</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cart;
+/*import { serverTimestamp } from "firebase/firestore"
 import React, { useContext, useState } from "react"
 import Form from "./Items/Form"
 import { cartContext } from "./CartContext";
@@ -53,7 +124,7 @@ const Cart = () =>{
                             </div>
                     ))}
                     <div>
-                        <h2>Total: ${precioTotal()}</h2>
+                        <h2>Total: $</h2>
                         <button onClick={borrarTodo}>Vaciar Carrito</button>
                     </div>
                 </div>
@@ -67,4 +138,4 @@ const Cart = () =>{
     </>
 )}
 
- export default Cart
+ export default Cart*/
