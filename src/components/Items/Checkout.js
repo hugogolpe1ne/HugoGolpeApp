7c387f5dc47a8f94902b2045c6b385d2e6b49711
hugo <hugo.golpe.1@gmail.com>
2022-07-25 17:31:16 -0300

    proyecto final

--- src/components/Items/Checkout.js
+++ src/components/Items/Checkout.js
@@ -1,33 +1,21 @@
 import React, { useState } from 'react';
-import { db } from '../../config/firebase';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
-import Form from "./Form";
+import { db } from "./Firebase"
+import Form from './Form';
 import { useContext } from 'react';
-import { cartContext } from '../../context/CartContext';
-import { userContext } from '../../context/UserContext';
+import { cartContext } from '../CartContext';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
 
 export const Checkout = () => {
-  const userContextResult = useContext(userContext);
   const resultContext = useContext(cartContext);
-  const userLoggedIn = userContextResult.userLogin;
-
   const [userData, setUserData] = useState({
     name: '',
     lastName: '',
     phone: '',
-    email: userLoggedIn ? userContextResult.userInfo.email : '',
+    email: '',
     confirmEmail: '',
   });
   const [orderId, setOrderId] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (userLoggedIn) {
-      toast.info(`Vas a comprar como: ${userContextResult.userInfo.email}`);
-    }
-  }, []);
 
   let cartCheckout = [];
   resultContext.carrito.forEach((element) => {
@@ -46,21 +34,13 @@ export const Checkout = () => {
     const target = e.target;
     const label = target.parentElement.children[0];
     if (value === '') {
-      label.classList.remove(`${style.activateLabel}`);
+      label.classList.remove();
     } else if (value !== '') {
-      label.classList.add(`${style.activateLabel}`);
+      label.classList.add();
     }
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (cartCheckout.length < 1) {
-      toast.error(
-        'No hay nada agregado al carrito. Por favor seleccione el producto que desea'
-      );
-      return;
-    }
-    setLoading(true);
     const orderData = {
       buyer: {
         name: userData.name,
@@ -78,47 +58,24 @@ export const Checkout = () => {
       .then((response) => {
         setOrderId(response.id);
         resultContext.clear();
-        setLoading(false);
-        toast.success('Compra exitosa');
       })
-      .catch((error) => {
-        setLoading(false);
-        toast.error(error.message);
+      .catch((error) => {;
       });
   };
 
   return (
     <div>
-       :orderId !== '' ? (
-        <div>
-          <h1>¡Muchas Gracias por tu compra!</h1>
-          <p>
-            Te enviamos un email a <em>{userData.email}</em> con el detalle de
-            tu orden de compra. Tu número de orden es:{' '}
-            <strong>{orderId}</strong>.
-            <br />
-            Esperamos que tu experiencia dentro de Electro App haya sido de lo
-            mejor.
-          </p>
-
-          <Link to="/">
-            Ir al home
-          </Link>
-        </div>
-      ) : (
-        <div>
           <h1>Checkout</h1>
           <div>
             <div>
               <h2>Resumen de compra</h2>
               <div>
-                <p>Items: {resultContext.totalQuantity}</p>
+                <p>Items: {resultContext.cantidadTotal}</p>
                 <p>Envio: ¡Gratis!</p>
-                <p>Total: ${resultContext.totalPrice}</p>
+                <p>Total: ${resultContext.precioTotal}</p>
               </div>
-              <Link to="/cart" className={style.backToCart}>
-                <span>arrow_back</span>
-                Volver al carrito
+              <Link to="/cart">
+                <span>Volver al carrito</span>
               </Link>
             </div>
             <div>
@@ -129,59 +86,10 @@ export const Checkout = () => {
                 handleChange={handleChange}
                 userData={userData}
                 handleSubmit={handleSubmit}
-                logged={userLoggedIn}
               />
             </div>
           </div>
         </div>
-      )
-    </div>
-  );
-};
-
-/*import { db } from "./Firebase"
-import { collection , addDoc , serverTimestamp } from "firebase/firestore"
-import { useState } from "react"
-
-
-const Checkout = () => {
-
-  const [idCompra,setIdCompra] = useState("")
-
-  const handleBuy = () => {
-    
-    const collectionOrdenes = collection(db,"ordenes")
-
-    const orderData = {
-      buyer : {
-        name : "Horacio",
-        phone : "555555555",
-        email : "test@tes.com"
-      },
-      items : [{id:1,titulo:"test producto"}],
-      date : serverTimestamp(),
-      total : 100
-    }
-
-    const consulta = addDoc(collectionOrdenes,orderData)
-
-    consulta
-      .then(resultado=>{
-        setIdCompra(resultado.id)
-      })
-      .catch(error=>{
-        console.log(error)
-      })
-
-
-  }
-
-  return (
-    <>
-    <div>Checkout</div>
-    {idCompra&&<p>Su compra es : {idCompra}</p>}
-    <button onClick={handleBuy}>guardar</button>
-    </>
   )
 }
-export default Checkout*/
+export default Checkout
--- src/components/Items/Form.js
+++ src/components/Items/Form.js
@@ -1,34 +1,58 @@
-import React from "react";
+import React from 'react';
 
-const Form = ({handleChange, data, handleSubmit}) => {
-    return (
+const Form = ({ handleChange, userData, handleSubmit, logged }) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    placeholder="Nombre"
-                    type="text"
-                    name="name"
-                    onChange={handleChange}
-                    value={data.name}
-                />
-                <input
-                    placeholder="Email"
-                    type="text"
-                    name="email"
-                    onChange={handleChange}
-                    value={data.email}
-                />
-                <input
-                    placeholder="Telefono"
-                    type="text"
-                    name="phone"
-                    onChange={handleChange}
-                    value={data.phone}
-                />
-                <button>Finalizar Compra</button>
-            </form>
+          <label>Nombre</label>
+          <input
+            type="text"
+            placeholder="Nombre"
+            name="name"
+            onChange={handleChange}
+            value={userData.name}
+            required
+          ></input>
         </div>
-    )
-}
+        <div>
+          <label>Apellido</label>
+          <input
+            type="text"
+            placeholder="Apellido"
+            name="lastName"
+            onChange={handleChange}
+            value={userData.lastName}
+            required
+          ></input>
+        </div>
+        <div>
+          <label>Teléfono</label>
+          <input
+            type="text"
+            placeholder="Teléfono (insertar mínimo 7 dígitos)"
+            name="phone"
+            onChange={handleChange}
+            value={userData.phone}
+            required
+          ></input>
+        </div>
+          <div>
+            <label>
+              E-mail
+            </label>
+            <input
+              type="email"
+              placeholder="E-mail"
+              name="email"
+            ></input>
+          </div>
+          <button>
+            Realizar compra
+          </button>
+      </form>
+    </div>
+  );
+};
 
 export default Form
